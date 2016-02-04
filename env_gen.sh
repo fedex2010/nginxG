d8d3b1f41fda5a909ea1f8930f115135880d8133
Mariano Cibelli <mcibelli@Marianos-MacBook-Pro.local>
2016-02-04 16:19:02 -0300

    chages

--- env_gen.sh
+++ env_gen.sh
@@ -1,2 +1,3 @@
 echo 'set $EPI_UI_DNS' $EPI_UI_DNS ';' > /etc/nginx/env.conf
 echo 'set $EPI_API_DNS' $EPI_API_DNS ';' >> /etc/nginx/env.conf
+sed -i "s/NEW_RELIC_KEY_PLACEHOLDER/$NEW_RELIC_KEY/g" "/etc/nginx-nr-agent/nginx-nr-agent.ini"
