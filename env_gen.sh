eb7df777d7d87452db2626c19892cc7e23fe5980
Mariano Cibelli <mcibelli@Marianos-MacBook-Pro.local>
2016-02-04 10:20:49 -0300

    fuck off, it finally works

--- env_gen.sh
+++ env_gen.sh
@@ -1,2 +1,2 @@
-echo 'set $EPI_UI_DNS' $EPI_UI_DNS ';' > env.conf
-echo 'set $EPI_API_DNS' $EPI_API_DNS ';' >> env.conf
+echo 'set $EPI_UI_DNS' $EPI_UI_DNS ';' > /etc/nginx/env.conf
+echo 'set $EPI_API_DNS' $EPI_API_DNS ';' >> /etc/nginx/env.conf
