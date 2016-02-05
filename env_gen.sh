4306fecdce18755679665a01495e946ee12a8ae3
Ruben Shimabuku <rushima@gmail.com>
2016-02-05 12:16:50 -0300

    add mapi proyect and X-Brand header

--- env_gen.sh
+++ env_gen.sh
@@ -1,3 +1,4 @@
 echo 'set $EPI_UI_DNS' $EPI_UI_DNS ';' > /etc/nginx/env.conf
 echo 'set $EPI_API_DNS' $EPI_API_DNS ';' >> /etc/nginx/env.conf
+echo 'set $MAPI_DNS' $MAPI_DNS ';' >> /etc/nginx/env.conf
 sed -i "s/NEW_RELIC_KEY_PLACEHOLDER/$NEW_RELIC_KEY/g" "/etc/nginx-nr-agent/nginx-nr-agent.ini"
