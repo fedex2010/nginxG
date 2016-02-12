6695727ea39d5fb26af8bfdef99080c750075d57
Mariano Cibelli <mcibelli@Marianos-MacBook-Pro.local>
2016-02-12 12:35:44 -0300

    configuring environment filtered for internal apps

--- env_gen.sh
+++ env_gen.sh
@@ -1,4 +1,5 @@
 echo 'set $EPI_UI_DNS' $EPI_UI_DNS ';' > /etc/nginx/env.conf
 echo 'set $EPI_API_DNS' $EPI_API_DNS ';' >> /etc/nginx/env.conf
 echo 'set $MAPI_DNS' $MAPI_DNS ';' >> /etc/nginx/env.conf
+echo 'set $API_CORE_DNS' $API_CORE_DNS ';' >> /etc/nginx/env.conf
 echo resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) ";" > /etc/nginx/resolvers.conf
