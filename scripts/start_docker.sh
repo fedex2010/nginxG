7fe0822edc672d8257c304e486d1459ce516035f
Nicolás Allegrotti <nallegrotti@garbarino.com.ar>
2016-02-14 18:16:36 -0300

    Corrige el script de inicialización

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,5 +1,5 @@
-container=$(docker ps -a | grep garba-nginx)
-if [[ "$container" -eq "" ]]; then
+container=$(docker ps -a | grep garba-nginx | sed s/.*garba-nginx/exists/)
+if [[ "$container" == "" ]]; then
 	echo "no existe -> se crea"
 	docker run -d -e EPI_UI_DNS=staging.garbarino.com -e EPI_API_DNS=staging.garbarino.com -e MAPI_DNS=mapi-staging.garbarino.com --name garba-nginx garba-nginx
 else
