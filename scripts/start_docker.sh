c877260a9a2dac6a1f386472af611386e833e00f
Nicolás Allegrotti <nallegrotti@garbarino.com.ar>
2016-02-16 00:41:37 -0300

    Agrega la url de api-core

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,7 +1,7 @@
 container=$(docker ps -a | grep garba-nginx | sed s/.*garba-nginx/exists/)
 if [[ "$container" == "" ]]; then
 	echo "no existe -> se crea"
-	docker run -d -e EPI_UI_DNS=staging.garbarino.com -e EPI_API_DNS=staging.garbarino.com -e MAPI_DNS=mapi-staging.garbarino.com --name garba-nginx garba-nginx
+	docker run -d -e EPI_UI_DNS=staging.garbarino.com -e EPI_API_DNS=staging.garbarino.com -e MAPI_DNS=mapi-staging.garbarino.com -e API_CORE_DNS=api-core-staging.garbarino.com  --name garba-nginx garba-nginx
 else
 	echo "existe -> lo arranca"
 	docker start garba-nginx
