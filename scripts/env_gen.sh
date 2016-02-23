6e2a5faa1dd30f3aa0af25edd58aee9e24e1d3c0
Nicolás Allegrotti <nallegrotti@garbarino.com.ar>
2016-02-23 14:08:07 -0300

    se generan upstreams para los 3 ambientes y se modifica el script de generación del environment para que use los upstreams que corresponden

--- scripts/env_gen.sh
+++ scripts/env_gen.sh
@@ -1,2 +1,3 @@
 cp /etc/nginx/env.conf.$APP_ENV /etc/nginx/env.conf
+cp /etc/nginx/conf.d/upstreams.conf.$APP_ENV /etc/nginx/conf.d/upstreams.conf
 echo resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) ";" > /etc/nginx/resolvers.conf
