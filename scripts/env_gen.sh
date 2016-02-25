f2417a003d8460e9de7750413afb155cab0670f9
Nicolás Allegrotti <nallegrotti@garbarino.com.ar>
2016-02-25 09:18:27 -0300

    Levanta el server de test para atajar el tráfico de NginX Mueve los proxy_pass a upstreams

--- scripts/env_gen.sh
+++ scripts/env_gen.sh
@@ -1,3 +1,16 @@
-cp /etc/nginx/env.conf.$APP_ENV /etc/nginx/env.conf
-cp /etc/nginx/conf.d/upstreams.conf.$APP_ENV /etc/nginx/conf.d/upstreams.conf
+echo "----------------------" >> /var/log/nginx/startup.log
+echo "[$(date)] Environment set to $APP_ENV" >> /var/log/nginx/startup.log
+echo "[$(date)] Environment variables: \n$(env)" >> /var/log/nginx/startup.log
+
+
+cp /etc/nginx/env.conf.$APP_ENV /etc/nginx/env.conf 2>> /var/log/nginx/startup.log
+echo "[$(date)] Using /etc/nginx/env.conf.$APP_ENV" >> /var/log/nginx/startup.log
+
+cp /etc/nginx/conf.d/upstreams.conf.$APP_ENV /etc/nginx/conf.d/upstreams.conf  2>> /var/log/nginx/startup.log
+echo "[$(date)] Using /etc/nginx/conf.d/upstreams.conf.$APP_ENV" >> /var/log/nginx/startup.log
+
+cp /etc/nginx/security/test-ips.conf.$APP_ENV /etc/nginx/security/test-ips.conf  2>> /var/log/nginx/startup.log
+echo "[$(date)] Using /etc/nginx/security/test-ips.conf.$APP_ENV" >> /var/log/nginx/startup.log
+
 echo resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) ";" > /etc/nginx/resolvers.conf
+echo "[$(date)] Environment startup completed" >> /var/log/nginx/startup.log
--- scripts/start_docker.sh
+++
@@ -1,8 +0,0 @@
-container=$(docker ps -a | grep garba-nginx | awk '{print $1}')
-if [[ "$container" == "" ]]; then
-	echo "no existe"
-else
-	echo "existe -> lo borra"
-	docker rm $container
-fi
-docker run -d -e APP_ENV=ci -p 80:80 -p 81:81 --name garba-nginx garba-nginx
