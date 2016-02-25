c0ee1e0d5a9b681f698d6380388fb6d2de92071c
Nicolás Allegrotti <nallegrotti@garbarino.com.ar>
2016-02-25 12:29:57 -0300

    Achica violentamente el test-server

---
+++ test-server/test-server.js
@@ -0,0 +1,20 @@
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (req, response) {
+ response.writeHead(200, {"Content-Type": "application/json"});
+ response.end(JSON.stringify({ 
+  	"headers": req.headers, 
+  	"url": req.url, 
+  	"method": req.method, 
+  	"host": req.hostname, 
+  	"protocol": req.protocol
+  }));
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(3000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:3000/");
