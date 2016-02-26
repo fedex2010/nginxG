fe8a10db7b018bee9a6e2d70ec3972f872220cca
Nicolás Allegrotti <nallegrotti@garbarino.com.ar>
2016-02-26 15:29:40 -0300

    [FIX] se movieron los upstreams y faltaba corregir el script de inicialización

--- scripts/env_gen.sh
+++ scripts/env_gen.sh
@@ -6,8 +6,8 @@ echo "[$(date)] Environment variables: \n$(env)" >> /var/log/nginx/startup.log
 cp /etc/nginx/env.conf.$APP_ENV /etc/nginx/env.conf 2>> /var/log/nginx/startup.log
 echo "[$(date)] Using /etc/nginx/env.conf.$APP_ENV" >> /var/log/nginx/startup.log
 
-cp /etc/nginx/conf.d/upstreams.conf.$APP_ENV /etc/nginx/conf.d/upstreams.conf  2>> /var/log/nginx/startup.log
-echo "[$(date)] Using /etc/nginx/conf.d/upstreams.conf.$APP_ENV" >> /var/log/nginx/startup.log
+cp /etc/nginx/upstreams.conf.$APP_ENV /etc/nginx/upstreams.conf  2>> /var/log/nginx/startup.log
+echo "[$(date)] Using /etc/nginx/upstreams.conf.$APP_ENV" >> /var/log/nginx/startup.log
 
 cp /etc/nginx/security/test-ips.conf.$APP_ENV /etc/nginx/security/test-ips.conf  2>> /var/log/nginx/startup.log
 echo "[$(date)] Using /etc/nginx/security/test-ips.conf.$APP_ENV" >> /var/log/nginx/startup.log
