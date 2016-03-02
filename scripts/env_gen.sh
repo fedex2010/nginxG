93101e2e3b9bb96ffdfa9c5124de4676b6ad089c
Mariano <mcibelli@garbarino.com>
2016-03-02 15:43:33 -0300

    change  to env

--- scripts/env_gen.sh
+++ scripts/env_gen.sh
@@ -1,3 +1,3 @@
-cp /etc/nginx/env.conf.$APP_ENV /etc/nginx/env.conf
-cp /etc/nginx-nr-agent/nginx-nr-agent.ini.$APP_ENV /etc/nginx-nr-agent/nginx-nr-agent.ini
+cp -rf /etc/nginx/env.conf.$APP_ENV /etc/nginx/env.conf
+cp -rf /etc/nginx-nr-agent/nginx-nr-agent.ini.$APP_ENV /etc/nginx-nr-agent/nginx-nr-agent.ini
 echo resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) ";" > /etc/nginx/resolvers.conf
