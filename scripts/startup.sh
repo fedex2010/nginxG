fb2e8e772708c977d2de0c94eed2597282462c3c
Nicolás Allegrotti <nallegrotti@garbarino.com.ar>
2016-03-08 15:14:22 -0300

    Agrega el agente de nginx en los 3 ambientes

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,8 +4,12 @@ echo "[$(date)] Running env_gen" >> /var/log/nginx/startup.log
 echo "[$(date)] Finished env_gen" >> /var/log/nginx/startup.log
 
 echo "[$(date)] Running NginX startup" >> /var/log/nginx/startup.log
-nginx -g 'daemon off;' 2>>/var/log/nginx/startup.err
+nginx 2>>/var/log/nginx/startup.err
 echo "[$(date)] Finished NginX startup" >> /var/log/nginx/startup.log
 
 echo "[$(date)] Running nginx newrelic service" >> /var/log/nginx/startup.log
 service nginx-nr-agent start 2>>/var/log/nginx/startup.err
+echo "[$(date)] Startup finished" >>/var/log/nginx/startup.log
+
+# Esto es para que no termine el container, seguro hay una mejor manera, pero todavía no la encontré
+sleep 600d
