e0f4ace2266e32a51ed75f482b3237f26b85614f
Nicolás Allegrotti <nallegrotti@garbarino.com.ar>
2016-03-12 08:45:31 -0300

    Soporta el testeo de llamadas por ssl

--- tests/test.upstream.sh
+++ tests/test.upstream.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+if [[ "$1" == "ssl" ]]
+then
+	method=$2
+	domain=$3
+	uri=$4
+	dest=$5
+	
+	actual_upstream=$(docker run --link garba-nginx:$domain test-nginx curl -v -X $method -H 'X-Forwarded-Proto: https' $domain$uri 2>&1 | grep "X-Proxy-Host" | awk '{print $3}' | sed 's/[^(a-z|_)]//g') 
+else
+	method=$1
+	domain=$2
+	uri=$3
+	dest=$4
+	
+	actual_upstream=$(docker run --link garba-nginx:$domain test-nginx curl -v -X $method $domain$uri 2>&1 | grep "X-Proxy-Host" | awk '{print $3}' | sed 's/[^(a-z|_)]//g') 
+fi
 
-method=$1
-domain=$2
-uri=$3
-dest=$4
-
-actual_upstream=$(docker run --link garba-nginx:$domain test-nginx curl -v -X $method "$domain$uri" 2>&1 | grep "X-Proxy-Host: $dest" )
-
-if [[ -n "$actual_upstream" ]]; then
+if [[ "$dest" == "$actual_upstream" ]]; then
 	echo -n ·
 else
 	echo
