56357baf3b2ae802272202d7d3c0ef24293e9074
Nicolás Allegrotti <nallegrotti@garbarino.com.ar>
2016-03-18 21:48:52 -0300

    Modifica la ejecución de tests para minimizar la cantidad de containers implicados en su ejecución

---
+++ tests/linked.domains.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ssl_domains=$(cat cases | grep -v '^##' | sort | awk '$2=="ssl" { print "--link garba-nginx:"$4 }' | uniq | tr '\n' ' ')
+no_ssl_domains=$(cat cases | grep -v '^##' | sort | awk '$2!="ssl" { print "--link garba-nginx:"$3 }' | uniq | tr '\n' ' ')
+
+linked_domains=$(echo "$ssl_domains $no_ssl_domains")
+echo $linked_domains
