1d6febc7828d38c2975c508ec25b4ab62833874c
Nicolás Allegrotti <nallegrotti@garbarino.com.ar>
2016-04-22 15:11:12 -0300

    Separa los cases por módulo para que sea un poco más mantenible

--- tests/run.cases.sh
+++ tests/run.cases.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /tests
-cat cases | grep -v "^#" | xargs -L 1 -P 15 bash && (echo && echo All tests successfully passed. Congratulations!! Your a great Engineer) || (echo "Something went wrong, check your changes" && exit -1)
+cat *.cases | grep -v "^#" | xargs -L 1 -P 15 bash && (echo && echo All tests successfully passed. Congratulations!! Your a great Engineer) || (echo "Something went wrong, check your changes" && exit -1)
