99512c995ba7d32d69a265b52da563a25e40f153
Mariano Tewel <mtewel@Marianos-MacBook-Pro.local>
2016-06-08 14:43:43 -0300

    Se agrega ebextentions para modificar la nginx.conf del host. Se lleva worker_connections de 1024 a 4096

---
+++ .ebextensions/scripts/nginx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR_TMP=/tmp/nginx
+DIR_EBEXTEN=$DIR_TMP/.ebextensions
+
+/etc/init.d/nginx stop
+cd /etc/nginx
+cp nginx.conf nginx.back
+cp $DIR_EBEXTEN/conf/nginx.conf .   
+
+#Borro directorio temporal
+rm -rf $DIR_TMP
