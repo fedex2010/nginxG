920efa3947d7f76ef16f59fe4fce58a8842dc8df
Mariano Tewel <mtewel@Marianos-MacBook-Pro.local>
2016-06-27 16:04:35 -0300

    Se agrega funcionalida para instalar awslogs en ambiente prod

--- .ebextensions/scripts/nginx.sh
+++ .ebextensions/scripts/nginx.sh
@@ -8,5 +8,29 @@ cd /etc/nginx
 cp nginx.conf nginx.back
 cp $DIR_EBEXTEN/conf/nginx.conf .   
 
+#Instalo awslogs, solo instalo si el ambiente es prod
+docker_id=$(docker ps -q)
+env=$(docker exec $docker_id env | grep APP_ENV | cut -d = -f2)
+
+if [ $env == "prod" ]
+then
+	rpm -qa | grep  awslogs
+	if [ $? -eq 1 ]
+	then
+		yum update -y
+		yum install -y awslogs
+	fi
+
+	#Para iniciar luego de reboot
+	chkconfig  awslogs on
+
+	cd /etc/awslogs
+	cp $DIR_EBEXTEN/conf/awslogs.conf .   
+
+#rm -f /var/log/eb-docker/containers/eb-current-app/*.log
+
+	/etc/init.d/awslogs start
+fi
+
 #Borro directorio temporal
 rm -rf $DIR_TMP
