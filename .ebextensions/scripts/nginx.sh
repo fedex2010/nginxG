4692e7566c23bc689c6025287e39e98fbbf74ab3
Mariano Tewel <mtewel@Marianos-MacBook-Pro.local>
2016-07-05 15:46:04 -0300

    Se cambia el script nginx.sh

--- .ebextensions/scripts/nginx.sh
+++ .ebextensions/scripts/nginx.sh
@@ -2,6 +2,11 @@
 
 DIR_TMP=/tmp/nginx
 DIR_EBEXTEN=$DIR_TMP/.ebextensions
+export EC2_HOME=/opt/aws/apitools/ec2
+export JAVA_HOME=/usr/lib/jvm/jre-1.7.0-openjdk.x86_64
+export AWS_PATH=/opt/aws
+export PATH=$PATH:$AWS_PATH/bin
+export CLASSPATH=$EC2_HOME/lib
 
 /etc/init.d/nginx stop
 cd /etc/nginx
@@ -9,10 +14,9 @@ cp nginx.conf nginx.back
 cp $DIR_EBEXTEN/conf/nginx.conf .   
 
 #Instalo awslogs, solo instalo si el ambiente es prod
-docker_id=$(docker ps -q)
-env=$(docker exec $docker_id env | grep APP_ENV | cut -d = -f2)
-
-if [ $env == "prod" ]
+env=$(ec2-describe-tags --filter "resource-type=instance" --filter "resource-id=$(ec2-metadata -i | /usr/bin/cut -d ' ' -f2)" --filter "key=Name" | /usr/bin/cut -f5)
+echo $env > /tmp/env.txt
+if [ $env == "nginx-prod" ]
 then
 	rpm -qa | grep  awslogs
 	if [ $? -eq 1 ]
