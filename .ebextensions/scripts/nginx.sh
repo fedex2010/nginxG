bc60e4c829990551b6736a134e6b9fcea540aa0a
Mariano Tewel <mtewel@Marianos-MacBook-Pro-2.local>
2016-09-12 16:25:11 -0300

    Se agrega conf para rotacion de logs

--- .ebextensions/scripts/nginx.sh
+++ .ebextensions/scripts/nginx.sh
@@ -8,9 +8,13 @@ cd /etc/nginx
 cp nginx.conf nginx.back
 cp $DIR_EBEXTEN/conf/nginx.conf .   
 
-cd /etc/logrotate.d
-cp $DIR_EBEXTEN/conf/docker .
-chmod 644 docker
+cd /etc
+cp $DIR_EBEXTEN/conf/log_rotate_docker.conf .
+chmod 644 log_rotate_docker.conf
+
+cd /etc/cron.hourly
+cp $DIR_EBEXTEN/conf/cron.logrotate.dockerlogs.conf .
+chmod 755 cron.logrotate.dockerlogs.conf
 
 #Borro directorio temporal
 #rm -rf $DIR_TMP
