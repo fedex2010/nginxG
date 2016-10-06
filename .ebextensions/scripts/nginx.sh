ff8a40c55cabdb145e8f448e5530d9da61d71f31
Mariano Tewel <mtewel@Marianos-MacBook-Pro-2.local>
2016-10-06 10:39:00 -0300

    Tuneo de nginx

--- .ebextensions/scripts/nginx.sh
+++ .ebextensions/scripts/nginx.sh
@@ -8,5 +8,11 @@ cd /etc/nginx
 cp nginx.conf nginx.back
 cp $DIR_EBEXTEN/conf/nginx.conf .   
 
+echo "fs.file-max = 70000" >> /etc/security/limits.conf
+echo "nginx soft nofile 10000" >> /etc/security/limits.conf
+echo "nginx hard nofile 50000" >> /etc/security/limits.conf
+
+/sbin/sysctl -p
+
 #Borro directorio temporal
 #rm -rf $DIR_TMP
