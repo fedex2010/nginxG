2078a528b2df331987851978a9f8be0e3d82f648
Mariano Tewel <mtewel@Marianos-MacBook-Pro-2.local>
2016-10-07 10:23:25 -0300

    No se hace stop del nginx en ebextensions

--- .ebextensions/scripts/nginx.sh
+++ .ebextensions/scripts/nginx.sh
@@ -3,7 +3,7 @@
 DIR_TMP=/tmp/nginx
 DIR_EBEXTEN=$DIR_TMP/.ebextensions
 
-/etc/init.d/nginx stop
+#/etc/init.d/nginx stop
 cd /etc/nginx
 cp nginx.conf nginx.back
 cp $DIR_EBEXTEN/conf/nginx.conf .   
