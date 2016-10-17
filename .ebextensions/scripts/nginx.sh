d30aa4898420df068eab15064565042af13010d4
Mariano Tewel <mtewel@Marianos-MacBook-Pro-2.local>
2016-10-17 10:45:07 -0300

    Cambios de conf en nginx

--- .ebextensions/scripts/nginx.sh
+++ .ebextensions/scripts/nginx.sh
@@ -3,7 +3,6 @@
 DIR_TMP=/tmp/nginx
 DIR_EBEXTEN=$DIR_TMP/.ebextensions
 
-#/etc/init.d/nginx stop
 cd /etc/nginx
 cp nginx.conf nginx.back
 cp $DIR_EBEXTEN/conf/nginx.conf .   
@@ -26,10 +25,19 @@ then
 	echo "nginx hard nofile 50000" >> /etc/security/limits.conf
 fi
 
-/sbin/sysctl -n net.ipv4.tcp_fin_timeout=30
-/sbin/sysctl -n net.ipv4.tcp_max_syn_backlog=3240000 
-/sbin/sysctl -n net.core.somaxconn=30000
-/sbin/sysctl -n net.ipv4.tcp_max_tw_buckets=1440000
+grep "custom-conf-start" /etc/sysctl.conf
+if [ $? == 1 ]
+then
+    printf "\n#custom-conf-start\n" >> /etc/sysctl.conf
+    printf "fs.file-max = 2037581\nnet.core.somaxconn = 65536\nnet.core.netdev_max_backlog = 65536\n" >> /etc/sysctl.conf
+    printf "\nnet.ipv4.tcp_fin_timeout = 30\nnet.ipv4.tcp_max_syn_backlog = 3240000\nnet.ipv4.tcp_max_tw_buckets = 1440000\n" >> /etc/sysctl.conf
+    printf "\nnet.ipv4.ip_forward = 1\nnet.ipv4.ip_local_port_range = 1024 65000\nnet.netfilter.nf_conntrack_max = 700000\n" >> /etc/sysctl.conf
+    printf "\n#custom-conf-end\n" >> /etc/sysctl.conf
+    sysctl -p
+fi
 
+WORKER_PROCS=$(getconf _NPROCESSORS_ONLN)
+
+sed -i "s/WORKER_PROCS/$WORKER_PROCS/g" /etc/nginx/nginx.conf
 #Borro directorio temporal
 #rm -rf $DIR_TMP
